bc59477b1993ea98e9ba58dae3f3dc3eb0dcd5e8
Lillard <lillard666@gmail.com>
2021-12-05 20:36:03 +0800

    update 23

--- problems/23/MergeKSortedLists.java
+++ problems/23/MergeKSortedLists.java
@@ -1,64 +1,32 @@
 /**
- * @(#)MergeKSortedLists.java, Jun 24, 2020.
- *
- * Copyright 2020 Lillard. All rights reserved. Use is subject to license terms.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
-
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
-/**
- * @author Lillard
- */
-public class MergeKSortedLists {
-
-  class Solution {
-
-    public class ListNode {
-
-      int val;
-      ListNode next;
-
-      ListNode() {
-      }
-
-      ListNode(int val) {
-        this.val = val;
-      }
-
-      ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-      }
-    }
-
+class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-      if (lists == null || lists.length <= 0) {
-        return null;
-      }
-
-      ListNode ans = new ListNode(0, null);
-      ListNode tmp = ans;
-
-      PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparing(a -> a.val));
-      for (int i = 0; i < lists.length; i++) {
-        if (lists[i] != null) {
-          queue.add(lists[i]);
+        Queue<ListNode> q = new PriorityQueue<>(Comparator.comparing(a -> a.val));
+        for (ListNode node : lists) {
+            if (node != null) {
+                q.add(node);
+            }
         }
-      }
-
-      while (queue.size() > 0) {
-        ListNode min = queue.poll();
-        if (min.next != null) {
-          queue.add(min.next);
+        ListNode head = new ListNode();
+        ListNode pre = head;
+        ListNode top = null;
+        while ((top = q.poll()) != null) {
+            pre.next = top;
+            if (top.next != null) {
+                q.add(top.next);
+            }
+            pre = pre.next;
         }
-
-        tmp.next = min;
-        tmp = tmp.next;
-        tmp.next = null;
-      }
-
-      return ans.next;
+        
+        return head.next;
     }
-  }
 }
